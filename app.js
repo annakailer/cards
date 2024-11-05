3e64ac90b4c5f9adccd7e056edfd913b128f3843
Anna Luiza <140816415+annakailer@users.noreply.github.com>
2024-11-05 08:12:16 -0300

    f

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ function criarCartao(categoria, pergunta, resposta) {
    cartao.className = 'cartao'
    cartao.innerHTML = `
    <div class="cartao_conteudo">
-                    <h3>Biologia</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao_conteudo_pergunta">
-                        <p>O que é genética? </p>
+                        <p>${pergunta} </p>
                     </div>
 
                     <div class="cartao_conteudo_resposta">
-                        <p>é o ramo da Biologia que estuda a hereditariedade, os genes e como as características são
-                            transmitidas de uma geração para outra. </p>
+                        <p>${resposta} </p>
                     </div>
 
                 </div>
