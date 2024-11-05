60a8a91a03dc77fa6b7e0003f2ec68009828ed79
Anna Luiza <140816415+annakailer@users.noreply.github.com>
2024-11-05 08:17:59 -0300

    c

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,53 @@
 criarCartao(
-    'Biologia',									
+    'Biologia',
     'O que é genética?',
     'é o ramo da Biologia que estuda a hereditariedade, os genes e como as características são transmitidas de uma geração para outra.'
-    )
+)
 criarCartao(
     'história',
-'Quem foi o responsável pela unificação da Alemanha em 1871 ?',
+    'Quem foi o responsável pela unificação da Alemanha em 1871 ?',
     'Otto von Bismarck, chanceler da Prússia'
 
 )
 criarCartao(
     'geografia',
-        'Qual o maior deserto do mundo ?',
+    'Qual o maior deserto do mundo ?',
     'Deserto do Saara'
-        )
+)
 criarCartao(
     'química',
-            'Qual o símbolo do ouro ?',
+    'Qual o símbolo do ouro ?',
     'Au'
-            )
+)
 criarCartao('Sociologia',
-                'o que é cultura ?',
+    'o que é cultura ?',
     'conjunto de práticas, valores e crenças de uma sociedade'
 
 )
 criarCartao('língua portuguesa',
-                    'o que é uma oração subordinada',
-                    'é uma oração que depende de outra pra ter um sentido completo.ex: Não saímos porque estava chovendo = oração subordinada')
+    'o que é uma oração subordinada',
+    'é uma oração que depende de outra pra ter um sentido completo.ex: Não saímos porque estava chovendo = oração subordinada')
 
 criarCartao('Inglês',
-                    'qual a forma correta do verbo “to be” no passado para “ele / ela”'?,
-'was'
-                    )
-criarCartao('Física', 
-                        'qual a fórmula para calcular a velocidade média ?',
+    'qual a forma correta do verbo “to be” no passado para “ele / ela”' ?,
+    'was'
+)
+criarCartao('Física',
+    'qual a fórmula para calcular a velocidade média ?',
     'Velocidade média = distância total / tempo total'
 )
 criarCartao('Ed.Financeira',
-                            'o que é fundo de emergência ?',
+    'o que é fundo de emergência ?',
     'é uma reserva para cobrir despesas imprevistas'
 )
 criarCartao('empreendedorismo',
-                                'o que é um MVP',
-                                'é um produto básico para testar uma ideia'
+    'o que é um MVP',
+    'é um produto básico para testar uma ideia'
 )
 criarCartao('robótica',
-                                    'O que faz um sensor ?',
-    'coleta dados do ambiente' 
-                                    )
+    'O que faz um sensor ?',
+    'coleta dados do ambiente'
+)
 criarCartao('biotecnologia',
-                                        'qual a primeira lei de mendel ?',
-        'As características dos indivíduos são determinadas por pares de fatores, os quais se separam na formação dos gametas, indo apenas um fator para cada gameta.')
+    'qual a primeira lei de mendel ?',
+    'As características dos indivíduos são determinadas por pares de fatores, os quais se separam na formação dos gametas, indo apenas um fator para cada gameta.')
