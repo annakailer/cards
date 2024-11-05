0f908bc9dd10e69eb73506cc4f32e215c36f5793
Anna Luiza <140816415+annakailer@users.noreply.github.com>
2024-11-05 07:51:14 -0300

    h

---
+++ perguntas.js
@@ -0,0 +1,5 @@
+criarCartao(
+    'Biologia'									
+    'O que é genética?'
+    'é o ramo da Biologia que estuda a hereditariedade, os genes e como as características são transmitidas de uma geração para outra.';
+    )
