ed1f78a83ef2fcdf7472b75e8f33c1a3ffd6598b
Anna Luiza <140816415+annakailer@users.noreply.github.com>
2024-11-05 08:10:30 -0300

    jdlk

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function criarCartao(categoria, pergunta, resposta) {
-   console.log(categoria, pergunta,resposta );
+   let cartao = document.createElement('article')
+   cartao.className = 'cartao'
+   cartao.innerHTML = `
+   <div class="cartao_conteudo">
+                    <h3>Biologia</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>O que é genética? </p>
+                    </div>
+
+                    <div class="cartao_conteudo_resposta">
+                        <p>é o ramo da Biologia que estuda a hereditariedade, os genes e como as características são
+                            transmitidas de uma geração para outra. </p>
+                    </div>
+
+                </div>
+   `
 }
--- perguntas.js
+++ perguntas.js
@@ -15,39 +15,39 @@ criarCartao(
     'Deserto do Saara'
         )
 criarCartao(
-    'química'
-            'Qual o símbolo do ouro ?'
+    'química',
+            'Qual o símbolo do ouro ?',
     'Au'
             )
-criarCartao('Sociologia'
-                'o que é cultura ?'
+criarCartao('Sociologia',
+                'o que é cultura ?',
     'conjunto de práticas, valores e crenças de uma sociedade'
 
 )
-criarCartao('língua portuguesa'
-                    'o que é uma oração subordinada'
+criarCartao('língua portuguesa',
+                    'o que é uma oração subordinada',
                     'é uma oração que depende de outra pra ter um sentido completo.ex: Não saímos porque estava chovendo = oração subordinada')
 
-criarCartao('Inglês' 
-                    'qual a forma correta do verbo “to be” no passado para “ele / ela”'?
+criarCartao('Inglês',
+                    'qual a forma correta do verbo “to be” no passado para “ele / ela”'?,
 'was'
                     )
-criarCartao('Física' 
-                        'qual a fórmula para calcular a velocidade média ?'
+criarCartao('Física', 
+                        'qual a fórmula para calcular a velocidade média ?',
     'Velocidade média = distância total / tempo total'
 )
-criarCartao('Ed.Financeira'
-                            'o que é fundo de emergência ?'
+criarCartao('Ed.Financeira',
+                            'o que é fundo de emergência ?',
     'é uma reserva para cobrir despesas imprevistas'
 )
-criarCartao('empreendedorismo' 
-                                'o que é um MVP'
+criarCartao('empreendedorismo',
+                                'o que é um MVP',
                                 'é um produto básico para testar uma ideia'
 )
-criarCartao('robótica'
-                                    'O que faz um sensor ?'
+criarCartao('robótica',
+                                    'O que faz um sensor ?',
     'coleta dados do ambiente' 
                                     )
-criarCartao('biotecnologia'
-                                        'qual a primeira lei de mendel ?'
+criarCartao('biotecnologia',
+                                        'qual a primeira lei de mendel ?',
         'As características dos indivíduos são determinadas por pares de fatores, os quais se separam na formação dos gametas, indo apenas um fator para cada gameta.')
